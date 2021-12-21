7481b54f582bb1d70f66ff89cde1a18e6187379c
raz <razholden11@gmail.com>
2021-12-21 22:14:38 +0200

    styling

--- src/App.js
+++ src/App.js
@@ -2,7 +2,7 @@ import logo from './logo.svg';
 import './App.css';
 import data from './Legends'
 import { useEffect, useState } from 'react'
-import {Button , Container , Row, Col} from 'react-bootstrap'
+import { Button, Container, Row, Col } from 'react-bootstrap'
 import 'bootstrap/dist/css/bootstrap.min.css';
 
 function App() {
@@ -20,52 +20,52 @@ function App() {
     setMonitors(data.Monitor)
   }, [])
 
-  const getMonitorsByMonitorType = (id,LegentId) => {
-   let MonitorsByMonitorType = Monitors.filter(x => x.MonitorTypeId == id)
-   setMonitorsByMonitorType([...MonitorsByMonitorType])
-   setLegendByMonitorType([])
-   setLegenId(LegentId)
+  const getMonitorsByMonitorType = (id, LegentId) => {
+    let MonitorsByMonitorType = Monitors.filter(x => x.MonitorTypeId == id)
+    setMonitorsByMonitorType([...MonitorsByMonitorType])
+    setLegendByMonitorType([])
+    setLegenId(LegentId)
   }
+  
   const getLegendsByMonitorType = () => {
     let Legend = Legends.find(x => x.Id == LegendId);
     let LegendTags = Legend.tags;
-    console.log(LegendTags)
     setLegendByMonitorType([...LegendTags])
   }
-  
+
   return (
     <div className="App">
 
-   <Container id = "buttonsMonitorType">
-   {
-   monitorType.map(x => {
-   return  <Button id = "buttonMonitorType"  variant="primary"  onClick = {() => getMonitorsByMonitorType(x.Id, x.LegentId)}>{x.Name}</Button> 
-   })
-
-   }
-  </Container>
-    <Container id = "buttonsMonitorType">
-   {
-     monitorsByMonitorType.length != 0 && 
-     monitorsByMonitorType.map(x => {
-       return  <Button   variant="primary"  onClick = {() => {getLegendsByMonitorType()}}>{x.Name}</Button>   
-     })
-
-   }
-        </Container> 
-       
+      <Container id="buttonsMonitorType">
         {
-        LegendByMonitorType.length != 0 && LegendByMonitorType.map(x => {
-        return  <Container> 
-          <Row>
-            <Col style = {{backgroundColor : x.Color}}></Col>
-            <Col>{x.Label}</Col>
-          </Row>
-          </Container> 
+          monitorType.map(x => {
+            return <Button id="buttonMonitorType" variant="primary" onClick={() => getMonitorsByMonitorType(x.Id, x.LegentId)}>{x.Name}</Button>
           })
-        }
-       
 
+        }
+      </Container>
+      <Container id="buttonsMonitor">
+        {
+          monitorsByMonitorType.length != 0 && monitorsByMonitorType.map(x => {
+            return <Button id="buttonMonitor" variant="primary" onClick={() => { getLegendsByMonitorType() }}>{x.Name}</Button>
+          })
+
+        }
+      </Container>
+      {
+        LegendByMonitorType.length != 0 && <Container id="Legends">
+          {
+            LegendByMonitorType.map(x => {
+              return <Container id="LegendTag">
+                <Row>
+                  <Col id="colorLegend" style={{ backgroundColor: x.Color }}></Col>
+                  <Col id="labelLegend">{x.Label}</Col>
+                </Row>
+              </Container>
+            })
+          }
+        </Container>
+      }
     </div>
   );
 }
